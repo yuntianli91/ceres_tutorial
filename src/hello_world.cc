dace628c7a2bbac4a598bb12ec9b4f6a41df556d
yuntianli <yuntianlee91@hotmail.com>
2024-04-18 15:11:51 +0800

    move CMakelists

--- src/hello_world.cc
+++ src/hello_world.cc
@@ -6,3 +6,35 @@
 // @date        Last modified at 2024-04-17
 // @copyright   Copyright (c) {2024 - } Purple Mountain Lab UAV
 
+#include <ceres/ceres.h>
+
+struct CostFunctor {
+  template <typename T>
+  bool operator()(const T* const x, T* residual) const {
+    residual[0] = 10.0 - x[0];
+    return true;
+  }
+};
+
+int main(int argc, char** argv){
+  // initial value
+  double x = 5.0;
+
+  ceres::Problem problem;
+
+  // 注意最新版本的ceres此处调用必须传入functor指针，否则会报错
+  ceres::CostFunction* cost_function =
+      new ceres::AutoDiffCostFunction<CostFunctor, 1, 1>(new CostFunctor);
+  problem.AddResidualBlock(cost_function, nullptr, &x);
+
+  ceres::Solver::Options option;
+  option.minimizer_progress_to_stdout = true;
+  ceres::Solver::Summary summary;
+  
+  ceres::Solve(option, &problem, &summary);
+
+  std::cout << summary.BriefReport() << "\n";
+  std::cout << "final x: " << x << "\n";
+
+  return 0;
+}
